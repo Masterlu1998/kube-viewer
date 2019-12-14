0d44f573ed7e03267aecd59b947966999dad6b36
robbin <lubing3302@gmail.com>
2019-12-14 22:13:11 +0800

    feat: change package tag

--- kube/cache.go
+++ kube/cache.go
@@ -5,21 +5,21 @@ import (
 
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/listers/apps/v1"
-	v12 "k8s.io/client-go/listers/batch/v1"
-	"k8s.io/client-go/listers/batch/v1beta1"
-	v13 "k8s.io/client-go/listers/core/v1"
+	appv1 "k8s.io/client-go/listers/apps/v1"
+	batchv1 "k8s.io/client-go/listers/batch/v1"
+	batchv1beta1 "k8s.io/client-go/listers/batch/v1beta1"
+	corev1 "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
 )
 
 type KubeLister struct {
-	DeploymentLister  v1.DeploymentLister
-	DaemonSetLister   v1.DaemonSetLister
-	StatefulSetLister v1.StatefulSetLister
-	ReplicaSetsLister v1.ReplicaSetLister
-	CronJobLister     v1beta1.CronJobLister
-	JobLister         v12.JobLister
-	NamespaceLister   v13.NamespaceLister
+	DeploymentLister  appv1.DeploymentLister
+	DaemonSetLister   appv1.DaemonSetLister
+	StatefulSetLister appv1.StatefulSetLister
+	ReplicaSetsLister appv1.ReplicaSetLister
+	CronJobLister     batchv1beta1.CronJobLister
+	JobLister         batchv1.JobLister
+	NamespaceLister   corev1.NamespaceLister
 }
 
 func NewKubeLister(ctx context.Context, client *kubernetes.Clientset) *KubeLister {
